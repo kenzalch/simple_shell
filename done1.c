3642c46855dd2a81f9eeadfa913f0bd8e99a4831
kenzalch <kenzalachgarensa@gmail.com>
2023-06-28 04:32:05 -0700

    done

---
+++ done1.c
@@ -0,0 +1,205 @@
+#include "main.h"
+
+/**
+ * _whch - Locates a command.
+ * @command: Command name.
+ * @_env: Environment variable.
+ *
+ * Return: Location of the command.
+ */
+char *_whch(char *command, char **_env)
+{
+	char *way_name, *ptrway, *tkn_way, *dir;
+	int lgth_dir, lgth_cmd, idx;
+	struct stat st;
+
+	way_name = get_environ("PATH", _env);
+	if (way_name)
+	{
+		ptrway = _strdup(way_name);
+		lgth_cmd = _strlen(command);
+		tkn_way = _strtok(ptrway, ":");
+		idx = 0;
+		while (tkn_way != NULL)
+		{
+			if (check_cdir(way_name, &index))
+				if (stat(command, &st) == 0)
+					return (command);
+			lgth_dir = _strlen(tkn_way);
+			dir = malloc(lgth_dir + lgth_cmd + 2);
+			_strcpy(dir, tkn_way);
+			_strcat(dir, "/");
+			_strcat(dir, command);
+			_strcat(dir, "\0");
+			if (stat(dir, &st) == 0)
+			{
+				free(ptrway);
+				return (dir);
+			}
+			free(dir);
+			tkn_way = _strtok(NULL, ":");
+		}
+		free(ptrway);
+		if (stat(command, &st) == 0)
+			return (command);
+		return (NULL);
+	}
+	if (command[0] == '/')
+		if (stat(command, &st) == 0)
+			return (command);
+	return (NULL);
+}
+
+/**
+ * check_cdir - Checks if ":" is present in the current directory.
+ * @filepath: Pointer to a character string representing the filepath.
+ * @index: Pointer to an integer representing the index.
+ *
+ * Return: 1 if the path is searchable in the current directory, 0 otherwise.
+ */
+int check_cdir(char *filepath, int *index)
+{
+	if (filepath[*index] == ':')
+		return (1);
+
+	while (filepath[*index] != ':' && filepath[*index])
+	{
+		*index += 1;
+	}
+
+	if (filepath[*index])
+		*index += 1;
+
+	return (0);
+}
+
+/**
+ * check_error_command - Verifies if the user has permissions
+ *                      to access a command or directory.
+ * @dir_dest: Destination directory.
+ * @datashell: Data structure.
+ *
+ * Return: 1 if there is an error, 0 if not.
+ */
+int check_error_command(char *dir_dest, shll_comm *datashell)
+{
+	if (dir_dest == NULL)
+	{
+		get_err(datashell, 127);
+		return (1);
+	}
+
+	if (_strcmp(datashell->args[0], dir_dest) != 0)
+	{
+		if (access(dir_dest, X_OK) == -1)
+		{
+			get_err(datashell, 126);
+			free(dir_dest);
+			return (1);
+		}
+		free(dir_dest);
+	}
+	else
+	{
+		if (access(datashell->args[0], X_OK) == -1)
+		{
+			get_err(datashell, 126);
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * command_exec - Executes command lines.
+ * @datashell: Data relevant (args and input).
+ *
+ * Return: 1 on success.
+ */
+int command_exec(shll_comm *datashell)
+{
+	pid_t pim, wpid;
+	int ste, exc;
+	char *dyr;
+	(void)wpid;
+
+	exc = is_exec(datashell);
+	if (exc == -1)
+		return (1);
+	if (exc == 0)
+	{
+		dyr = _whch(datashell->args[0], datashell->_env);
+		if (check_error_command(dir, datashell) == 1)
+			return (1);
+	}
+
+	pim = fork();
+	if (pim == 0)
+	{
+		if (exc == 0)
+			dyr = _whch(datashell->args[0], datashell->_env);
+		else
+			dyr = datashell->args[0];
+		execve(dyr + exc, datashell->args, datashell->_env);
+	}
+	else if (pim < 0)
+	{
+		perror(datashell->argv[0]);
+		return (1);
+	}
+	else
+	{
+		do {
+			wpid = waitpid(pim, &ste, WUNTRACED);
+		} while (!WIFEXITED(stte) && !WIFSIGNALED(stte));
+	}
+
+	datashell->stat = ste / 256;
+
+	return (1);
+}
+
+/**
+ * is_exec - Determines if a command is an executable.
+ * @datashell: Data structure.
+ *
+ * Return: 0 if it's not an executable, otherwise a positive number.
+ */
+int is_exec(shll_comm *datashell)
+{
+	struct stat status;
+	int idx;
+	char *ip;
+
+	ip = datashell->args[0];
+	for (idx = 0; inp[idx]; idx++)
+	{
+		if (ip[idx] == '.')
+		{
+			if (ip[idx + 1] == '.')
+				return (0);
+			if (ip[idx + 1] == '/')
+				continue;
+			else
+				break;
+		}
+		else if (ip[idx] == '/' && idx != 0)
+		{
+			if (ip[idx + 1] == '.')
+				continue;
+			idx++;
+			break;
+		}
+		else
+			break;
+	}
+	if (idx == 0)
+		return (0);
+
+	if (stat(ip + idx, &status) == 0)
+		return (idx);
+	get_err(datashell, 127);
+
+	return (-1);
+}
---
+++ done2.c
@@ -0,0 +1,157 @@
+#include "main.h"
+
+/**
+ * exit_sh - Exits the shell
+ * @datashell: Data structure containing relevant data
+ *             (status and args)
+ *
+ * Return: 0 on success.
+ */
+int exit_sh(shll_comm *datashell)
+{
+	unsigned int ustt;
+	int is_d, strl, bg_n;
+
+	if (datashell->args[1] != NULL)
+	{
+		ustt = _atoi(datashell->args[1]);
+		is_d = _isdigit(datashell->args[1]);
+		strl = _strlen(datashell->args[1]);
+		bg_n = ustt > (unsigned int)INT_MAX;
+		if (!is_d || strlen > 10 || bg_n)
+		{
+			get_err(datashell, 2);
+			datashell->stat = 2;
+			return (1);
+		}
+		datashell->stat = (ustat % 256);
+	}
+	return (0);
+}
+
+/**
+ * execute_line - Finds built-ins and commands.
+ * @datash: Data relevant (args).
+ *
+ * Return: 1 on success.
+ */
+int execute_line(shll_comm *datash)
+{
+	int (*builtin)(shll_comm *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_bltn(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (command_exec(datash));
+}
+
+/**
+ * err_environ - generates an error message for env-related operations.
+ * @data_sh: data relevant to the shell (counter, arguments).
+ *
+ * Return: Error message string.
+ */
+char *err_environ(shll_comm *data_sh)
+{
+	int lgth;
+	char *er, *txto, *vtr;
+
+	vtr = conv_itoa(data_sh->counter);
+	txto = ": Unable to add/remove from environment\n";
+	lgth = _strlen(data_sh->argv[0]) + _strlen(vtr);
+	lgth += _strlen(data_sh->args[0]) + _strlen(txto) + 4;
+	er = malloc(sizeof(char) * (lgth + 1));
+	if (er == 0)
+	{
+		free(er);
+		free(vtr);
+		return (NULL);
+	}
+
+	_strcpy(er, data_sh->argv[0]);
+	_strcat(er, ": ");
+	_strcat(er, vtr);
+	_strcat(er, ": ");
+	_strcat(er, data_sh->args[0]);
+	_strcat(er, txt);
+	_strcat(er, "\0");
+	free(vtr);
+
+	return (er);
+}
+
+/**
+ * err_path126 - generates an error message for path-related
+ *                operations with permission denied.
+ * @data_sh: data relevant to the shell (counter, arguments).
+ *
+ * Return: The error message string.
+ */
+char *err_path126(shll_comm *data_sh)
+{
+	int lgth;
+	char *vtr, *er;
+
+	vtr = conv_itoa(data_sh->counter);
+	lgth = _strlen(data_sh->argv[0]) + _strlen(vtr);
+	lgth += _strlen(data_sh->args[0]) + 24;
+	er = malloc(sizeof(char) * (lgth + 1));
+	if (er == 0)
+	{
+		free(er);
+		free(vtr);
+		return (NULL);
+	}
+	_strcpy(er, data_sh->argv[0]);
+	_strcat(er, ": ");
+	_strcat(er, vtr);
+	_strcat(er, ": ");
+	_strcat(er, data_sh->args[0]);
+	_strcat(er, ": Permission denied\n");
+	_strcat(er, "\0");
+	free(vtr);
+	return (er);
+}
+
+/**
+ * conc_err_msg - concatenates an error message for the 'cd' command.
+ * @data_sh: data relevant to the shell (directory, arguments).
+ * @disp_msg: message to be displayed.
+ * @err: output message buffer.
+ * @vstr: counter lines.
+ *
+ * Return: The concatenated error message.
+ */
+char *conc_err_msg(shll_comm *data_sh, char *disp_msg, char *err, char *vstr)
+{
+	char *ill_flg;
+
+	_strcpy(err, data_sh->argv[0]);
+	_strcat(err, ": ");
+	_strcat(err, vstr);
+	_strcat(err, ": ");
+	_strcat(err, data_sh->args[0]);
+	_strcat(err, disp_msg);
+	if (data_sh->args[1][0] == '-')
+	{
+		ill_flg = malloc(3);
+		ill_flg[0] = '-';
+		ill_flg[1] = data_sh->args[1][1];
+		illegal_flag[2] = '\0';
+		_strcat(err, ill_flg);
+		free(ill_flg);
+	}
+	else
+	{
+		_strcat(err, data_sh->args[1]);
+	}
+
+	_strcat(err, "\n");
+	_strcat(err, "\0");
+	return (err);
+}
